0af889e60636ebd60e8d2b8fe2a5b16c7a883c98
Paul Conrad <pjc@pauljconrad.net>
2016-04-09 13:21:19 -0700

    Added comment about returning from program

--- first/first.s
+++ first/first.s
@@ -2,7 +2,7 @@
 .global main // main is our entry point to program and must be global
 
 main:
-	push {lr}     	// push address of caller on stack
+	push {lr}   // push address of caller on stack
 	mov r0, #3	// load the value 3 into r0 register
 	add r0, #7	// add immediate value of 7 to r0, store result in r0 register
-	pop {pc}	// return from main
+	pop {pc}	// return from main - several different ways we can exit from program
