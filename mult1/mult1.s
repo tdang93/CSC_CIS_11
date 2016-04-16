ba816b49ec8f0d30c207a3203614abb34097dc67
Paul Conrad <pjc@pauljconrad.net>
2016-04-16 11:39:12 -0700

    New commits

--- mult1/mult1.s
+++ mult1/mult1.s
@@ -3,7 +3,7 @@
 .balign 4
 .data
 
-value1: .word 3
+value1: .word 40
 
 .balign 4
 .text
@@ -11,6 +11,6 @@ main:
 	push {lr}	// save link register (return address)
 	ldr r1, =value1 // r1 contains address of value1
 	ldr r1, [r1]	// r1 now contains value at value1
-	add r1, r1,lsl #2 // see if r1=r1*5
+	add r1,r1,lsl #2 // see if r1=r1*5
 	mov r0, r1	// put result of r1 into r0 for return value
 	pop {pc}	// exit program
