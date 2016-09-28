6e30521bdf41af48569d5110924f2abbf0a7cacd
Paul Conrad <paul@pauljconrad.net>
2016-09-28 13:38:10 -0700

    Commits for 09282016_1338

---
+++ bare_bones/bare_bones.s
@@ -0,0 +1,14 @@
+// bare bones source file 
+.global main
+
+//.align 4
+.text
+main: 	push {lr} // save link register, this is one of many ways this can be done
+
+	// assembly program here
+
+	mov r0, #1024 // return code for your program (must be 8 bits)
+	pop {pc}
+
+.data
+	// all program data goes here, variables, constants, etc
